058adf20dfad78102bfe9e470712fc649b687249
Bella Mironova <bellamironova@gmail.com>
2022-09-01 23:02:20 +0300

    deleted crash-test

--- backend/src/configs/index.js
+++ backend/src/configs/index.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 
 const { PORT = 3001, JWT_SECRET = 'f7585a14c9e90b0058253652d000dbf34029c94925d6e9dc8286ff326823d2d5' } = process.env;
 const SALT_ROUND = 10;
+const JWT_STORAGE_TIME = '7d';
 const DATABASE_URL = 'mongodb://127.0.0.1:27017/mestodb';
 
 module.exports = {
   SALT_ROUND,
   JWT_SECRET,
+  JWT_STORAGE_TIME,
   PORT,
   DATABASE_URL,
 };
