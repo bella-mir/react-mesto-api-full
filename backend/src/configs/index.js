70bed7650d9bb304f73264cbd777a5427b8cbfde
Bella Mironova <bellamironova@gmail.com>
2022-09-01 12:44:14 +0300

    Fixed JWT_key

--- backend/src/configs/index.js
+++ backend/src/configs/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { JWT_SECRET = 'The most s3cr3t key' } = process.env;
+const { JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b' } = process.env;
 
 const SALT_ROUND = 10;
 
